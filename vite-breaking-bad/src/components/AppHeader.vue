6d3145ab9ab64cbc8726c4c55d7e022b373955bf
Karim <kizpucci@gmail.com>
2022-11-17 23:22:25 +0100

    header-with-problem

---
+++ vite-breaking-bad/src/components/AppHeader.vue
@@ -0,0 +1,14 @@
+<script>
+export default {
+  name: "AppHeader",
+};
+</script>
+
+<template>
+  <div class="container">
+    <h1>BREAKING BAD</h1>
+  </div>
+</template>
+
+<style lang='scss' scoped>
+</style>
---
+++ vite-breaking-bad/src/components/AppSearch.vue
@@ -0,0 +1,12 @@
+<script>
+export default {
+  name: "AppSearch",
+};
+</script>
+
+<template>
+  <div class="container">AppSearch</div>
+</template>
+
+<style lang='scss' scoped>
+</style>
